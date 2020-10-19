e2281985480a57f04821f4c739aaa9ae7d08bf99
Your Name <daikiclimate041@gmail.com>
2020-10-19 11:10:47 +0900

    fix

--- utils.py
+++ utils.py
@@ -10,36 +10,48 @@ def make_feature(df_train, df_test):
     train_num = len(df_train)
     df = pd.concat([df_train.copy(), df_test.copy()])
 
-    df = pd.merge(df, stage_area, how = "left", on = "stage")
-    
 
-    #カテゴリカル化
     #print(len(df))
-    cat_cols = ["mode"]
-    for c in cat_cols:
-        vv, obj = pd.factorize(df[c])
-        df[c] = vv
-        df[c] = df[c].astype('category')
 
     #modeごとのweaponのcount
     df = count_x(df)
 
-    #target encode
-    df = target_x(df, train_num)
-
+    #target encoding
+    tgt_cols = ["count_uid",
+            ]
+    df = target_encodes(df, train_num, tgt_cols)
+ 
 
     #make features
     t3  = time.time()
     print("mkf:", round(t3-t2,1))
     return df[:train_num], df[train_num:]
 
-def target_x(df, train_num, target) 
-    #greedy target encode
-    ts = df.groupby(["x"], as_index=False)["target"].mean()
-    df = pd.merge(df, ts.rename(columns = {target:  "x"+ "_tgt"}), left_on ="x" , how = "left")
+def target_encodes(df, train_num, tgt_cols):
+    for c in tgt_cols:
+        data_tmp = pd.DataFrame({c:df[:train_num][c], "target":df[:train_num].target})
+        #validationの置換
+        target_mean = data_tmp.groupby(c)["target"].mean()
+        df.loc[train_num:, "tgt_" + c] = df[train_num:][c].map(target_mean)
+
+        #学習データ
+        tmp = np.repeat(np.nan, len(df[:train_num]))
+        kf_encoding = KFold(n_splits = 4, shuffle = False, random_state = 0)
+
+        # for train_index, valid_index in TimeSeriesSplit(n_splits=n_splits).split(np.arange(len(train))):
+        for idx_1, idx_2 in kf_encoding.split(df[:train_num]):
+            target_mean = data_tmp.iloc[idx_1].groupby(c)["target"].mean()
+            tmp[idx_2] = df.loc[:train_num].loc[idx_2][c].map(target_mean)
+        df[:train_num]["tgt_"+c] = tmp
     return df
 
-def count_x(df):
-    df = df.assign(new_col_name=0).groupby(["x"])["new_col_name"].count().reset_index()
-        #greedy target encode
+def count_encodes(df, tgt_cols):
+    for c in tgt_cols:
+        df["cnt_" + c] = df[c].map(df.Age.value_counts())
     return df
+
+def count_multi_encodes(df,tgt_cols):
+    for c in tgt_cols:
+        df["cnt_" + c[0] + "_" + c[1] = df.groupby([c[0], c[1]])["target"].transform("count")  # ←"Survived"は他の列名でもいい
+    return df
+
